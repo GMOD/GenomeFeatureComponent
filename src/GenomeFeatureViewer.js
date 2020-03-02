6dc839e817f938e0d331f4bd7e414d2daddbc0e6
Nathan Dunn <nathandunn@lbl.gov>
2020-03-02 14:38:36 -0800

    added a rough table structure

--- src/GenomeFeatureViewer.js
+++ src/GenomeFeatureViewer.js
@@ -32,7 +32,30 @@ export default class GenomeFeatureViewer {
     }
 
     generateLegend(){
-        let returnString = `<div><h3>CAts</h3></div>`;
+        let returnString = `<table><tbody>`;
+        returnString += `<tr>`;
+        returnString += `<td>Variant types:</td>`;
+        returnString += `<td colspan="2">Molecular Consequences:</td>`;
+        returnString += `</tr>`;
+        returnString += `<tr>`;
+        returnString += `<td><ul>`;
+        returnString += `<li>point mutation /MNV etc. </li>`;
+        returnString += `<li>deletion etc. </li>`;
+        returnString += `<li>insertion etc. </li>`;
+        returnString += `<li>delins etc. </li>`;
+        returnString += `</ul></td>`;
+        returnString += `<td><ul>`;
+        returnString += `<li>transcript ablation </li>`;
+        returnString += `<li>splice acceptor variant</li>`;
+        returnString += `</ul></td>`;
+        returnString += `<td><ul>`;
+        returnString += `<li>protein altering variant</li>`;
+        returnString += `<li>splice region variant </li>`;
+        returnString += `</ul></td>`;
+
+        returnString += `</tr>`;
+
+        returnString += `</tbody></table>`;
 
 
         return returnString;
